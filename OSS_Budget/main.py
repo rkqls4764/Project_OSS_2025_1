cf06b9ac860a1b62a588691d63535a34d20f19e7
gabeen <rkqls4764@naver.com>
2025-06-05 14:05:40 +0900

    [기능추가] 가계부 메뉴에 수입 옵션 추가

--- OSS_Budget/main.py
+++ OSS_Budget/main.py
@@ -6,30 +6,75 @@ def main():
 
     while True:
         print("==== 간단 가계부 ====")
-        print("1. 지출 추가")
-        print("2. 지출 목록 보기")
-        print("3. 총 지출 보기")
-        print("4. 종료")
+        print("1. 지출 관련")
+        print("2. 수입 관련")
+        print("3. 종료")
         choice = input("선택 > ")
 
         if choice == "1":
-            category = input("카테고리 (예: 식비, 교통 등): ")
-            description = input("설명: ")
-            try:
-                amount = int(input("금액(원): "))
-            except ValueError:
-                print("잘못된 금액입니다.\n")
-                continue
-            budget.add_expense(category, description, amount)
+            while True:
+                print("==== 지출 관련 ====")
+                print("1. 추가")
+                print("2. 목록 보기")
+                print("3. 총합 보기")
+                print("4. 뒤로 가기")
+                choice = input("선택 > ")
+                
+                if choice == "1":
+                    category = input("카테고리 (예: 식비, 교통 등): ")
+                    description = input("설명: ")
+                    try:
+                        amount = int(input("금액(원): "))
+                    except ValueError:
+                        print("잘못된 금액입니다.\n")
+                        continue
+                    budget.add_expense(category, description, amount)
+
+                elif choice == "2":
+                    budget.list_expenses()
+
+                elif choice == "3":
+                    budget.total_spent()
+
+                elif choice == "4":
+                    break
+
+                else:
+                    print("잘못된 선택입니다.\n")
 
         elif choice == "2":
-            budget.list_expenses()
+            while True:
+                print("==== 수입 관련 ====")
+                print("1. 추가")
+                print("2. 목록 보기")
+                print("3. 총합 보기")
+                print("4. 뒤로 가기")
+                choice = input("선택 > ")
+                
+                if choice == "1":
+                    category = input("카테고리 (예: 식비, 교통 등): ")
+                    description = input("설명: ")
+                    try:
+                        amount = int(input("금액(원): "))
+                    except ValueError:
+                        print("잘못된 금액입니다.\n")
+                        continue
+                    budget.add_income(category, description, amount)
+
+                elif choice == "2":
+                    budget.list_incomes()
+
+                elif choice == "3":
+                    budget.total_income()
+
+                elif choice == "4":
+                    break
+
+                else:
+                    print("잘못된 선택입니다.\n")
 
         elif choice == "3":
-            budget.total_spent()
-
-        elif choice == "4":
-            print("가계부를 종료합니다.")
+            print("가계부를 종료합니다.\n")
             break
 
         else:
